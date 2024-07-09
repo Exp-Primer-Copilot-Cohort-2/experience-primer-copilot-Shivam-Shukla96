f810e5daf6f08ca9abacd15538b6de2d082f95e6
Shivam Shukla <139495371+Shivam-Shukla96@users.noreply.github.com>
2024-07-09 07:51:02 +0000

    copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,11 @@
 function SkillsMember() {
     return (
-      <div>
-        <h1>Skills Member</h1>
+        <div>
+            <h1>Skills Member</h1>
+            <YourComponent /> {/* Replace YourComponent with the name of your component */}
+        </div>
+    );
+}
       </div>
     );
   }
