d6fd7d3d66bba6af37aed96ee1e8472b20c3dc70
Shivam Shukla <139495371+Shivam-Shukla96@users.noreply.github.com>
2024-07-09 07:43:08 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,8 @@
 function SkillsMember() {
-  return (
-    <div>
-      <h1>Skills Member</h1>
-    </div>
-  );
-}
-  
+    return (
+      <div>
+        <h1>Skills Member</h1>
+      </div>
+    );
+  }
+    
